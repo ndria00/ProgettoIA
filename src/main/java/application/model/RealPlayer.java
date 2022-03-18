de767513da1dbb52ac0fe15c21efce932b4c4f26
ndria00 <ctrndr00e01i872o@studenti.unical.it>
2022-03-18 15:03:43 +0100

    modified model... Installing Ubuntu

--- src/main/java/application/model/RealPlayer.java
+++ src/main/java/application/model/RealPlayer.java
@@ -1,9 +1,15 @@
 package application.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RealPlayer extends Player{
-
+	private ArrayList<Card> selectedCards;
+	
+	public RealPlayer() {
+		selectedCards =  new ArrayList<Card>();
+	}
+	
 	@Override
 	public void play(List<Play> plays) {
 		Game.getInstance().playerPlayed(null, plays);
@@ -11,6 +17,12 @@ public class RealPlayer extends Player{
 
 	@Override
 	public boolean canPlay(List<Play> availablePlays) {
+		HandOfCards handOfSelectedCards = new HandOfCards();
+		for (Card c: selectedCards) {
+			handOfSelectedCards.add(c);
+		}
+		//Call asp program that will try to play all the cards
+		//if some of the cards cannot be played then it will 
 		// TODO Auto-generated method stub
 		return false;
 	}
