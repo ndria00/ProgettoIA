5f507aa965916b5fcc9c5af457ccd587ee5dd649
Lebonsky <92588858+Lebonsky@users.noreply.github.com>
2022-04-22 18:52:35 +0200

    added bot cards panel and PlayView controller

--- src/main/java/application/controller/PlayViewController.java
+++ src/main/java/application/controller/PlayViewController.java
@@ -1,33 +1,74 @@
 package application.controller;
 
-import java.awt.event.MouseEvent;
 
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.event.MouseInputListener;
+
+import application.CardToImage;
+import application.model.Card;
+import application.model.Game;
+import application.model.Play;
+import application.view.CardPanel;
+import application.view.GameSpot;
+import application.view.GameSpotsPanel;
+import application.view.PlayView;
+import application.view.PlayerCardsPanel;
 
 public class PlayViewController implements MouseInputListener{
 	
 	
 	
+	PlayView playView = null;
+	GameSpotsPanel gameSpotsPanel = null;
 	
+	
+	
+	public PlayViewController(PlayView playView, GameSpotsPanel gameSpotPanel) {
+		this.playView = playView;
+	}
+	
+	public void setGameSpotsPanel(GameSpotsPanel gameSpotsPanel) {
+		this.gameSpotsPanel = gameSpotsPanel;
+	}
+	
+	
+	
+	public void updateGameSpots() {
+		if(this.gameSpotsPanel != null) {
+			for(Play play : Game.getInstance().getPlays()) {
+				for(GameSpot gs : this.gameSpotsPanel.getGameSpots()) {
+					gs.removeAll();
+					for(Card card : play) {
+						ImageIcon image = new ImageIcon();
+						image.setImage(CardToImage.getInstance().getImageFromCard(card).getImage());
+						CardPanel cardpanel = new CardPanel(image, PlayerCardsPanel.getInstance());
+						cardpanel.setCard(card);
+						gs.add(cardpanel);
+					}
+				}
+			}
+		}
+		System.out.println("UPDATED");
+	}
 	
 
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		updateGameSpots();
 	}
 
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -45,5 +86,7 @@ public class PlayViewController implements MouseInputListener{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 
 }
